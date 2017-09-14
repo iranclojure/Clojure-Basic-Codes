98be9e2aaf1edc9286499a77bd86f98c43c245c7
amirrezaask <raskarpour@gmail.com>
2017-09-15 01:40:57 +0430

    fixed a mistake in comments

--- binding/src/binding/core.clj
+++ binding/src/binding/core.clj
@@ -1,10 +1,13 @@
 (ns binding.core)
 
-(def myname "myname");You can access symbols that are defined by "def" keyword in all program.
+(def hisname "hisname");if you declare your symbol outside a function it will be global
+
 (defn -main []
+  (def myname "myname");You can access symbols that are defined by "def" keyword in whole scope of the function.
   (println myname)
   (let [yourname "yourname"];you can access symbols that are defined by "let" only in let scope
     (println yourname)
     );end of let scope
-  ;(println yourname) ;uncomment this code and check , you will get an error while compiling
+  ;(println yourname) ;uncomment this code and check , you will get an error while compiling    (println hisname)
+  (println hisname)
   )
