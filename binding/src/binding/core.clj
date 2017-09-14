77caecc704a88f74d29d71b5d469ea7a74d29c24
amirrezaask <raskarpour@gmail.com>
2017-09-15 01:46:39 +0430

    broke the code into functions for better understanding

--- binding/src/binding/core.clj
+++ binding/src/binding/core.clj
@@ -1,13 +1,21 @@
 (ns binding.core)
+;defn will create a function
+;always main function starts the program
 
-(def hisname "hisname");if you declare your symbol outside a function it will be global
-
-(defn -main []
-  (def myname "myname");You can access symbols that are defined by "def" keyword in whole scope of the function.
-  (println myname)
-  (let [yourname "yourname"];you can access symbols that are defined by "let" only in let scope
-    (println yourname)
+(defn learn-let []
+  (let [learnlet "learnlet"];you can access symbols that are defined by "let" only in let scope
+    (println learnlet)
     );end of let scope
   ;(println yourname) ;uncomment this code and check , you will get an error while compiling    (println hisname)
-  (println hisname)
+  )
+
+
+(defn learn-def []
+  (def learndef "learndef");You can access symbols that are defined by "def" keyword in whole scope of the function.
+  (println learndef)
+  )
+
+(defn -main []
+  (learn-def)
+  (learn-let)
   )
