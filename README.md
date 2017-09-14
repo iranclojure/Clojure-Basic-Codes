a20cb8597fcbe9457188939b9d3ae1ca20f47f23
amirrezaask <raskarpour@gmail.com>
2017-09-14 23:49:14 +0430

    Updated README and added hello world example

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Clojure Basic Codes</h1>
+<h2></h2>
+<h3>1.HelloWorld</h3>
---
+++ helloworld/src/helloworld/core.clj
@@ -0,0 +1,8 @@
+(ns helloworld.core);this is namespace decleration,
+;every clojure code should have namespace so other clojure codes can require it.
+
+
+(defn -main
+  [];this is a vector which declares what arguments this function requires
+  (println "HelloWorld")
+  )
